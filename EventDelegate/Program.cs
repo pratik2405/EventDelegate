0ca9e1c3c99587ab763e6ebf20c377761a37bb1e
pratik2405 <shinde7388pratik@gmail.com>
2024-11-14 12:34:31 +0530

    Add project files.

---
+++ EventDelegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDelegate
+{
+    public delegate void DisplayMsg();
+    internal class Program
+    {
+        static void PassMsg()
+        {
+            Console.WriteLine("You are pass");
+        }
+        static void FailMsg()
+        {
+            Console.WriteLine("You are fail !");
+        }
+        static void Main(string[] args)
+        {
+            Student stud=new Student();
+            stud.pass += new DisplayMsg(PassMsg);
+            stud.fail += new DisplayMsg(FailMsg);
+
+            stud.AcceptMarks(50);
+        }
+    }
+}
---
+++ EventDelegate/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDelegate
+{
+    public class Student
+    {
+        public event DisplayMsg pass;
+        public event DisplayMsg fail;
+
+        public void AcceptMarks(int mark)
+        {
+            if (mark >= 40)
+            {
+                pass();
+            }
+            else
+            {
+                fail();
+            }
+        }
+    }
+}
